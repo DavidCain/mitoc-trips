c4da1c1c28058a08d1f871a15f0e8274edf34faf
David Cain <davidjosephcain@gmail.com>
2014-09-10 23:34:38 -0400

    Change how leaders are stored in database
    Leader records just point to participant record for any field that is
    not redundant.
    
    See the docstring for the Leader model for more information and a
    rationalization of this behavior.

--- ws/models.py
+++ ws/models.py
@@ -36,19 +36,38 @@ class EmergencyInfo(models.Model):
     medical_history = models.TextField(blank=True, help_text="Anything your trip leader would want to know about.")
 
 
-class TripGoer(Person):
-    """ Anyone going on a trip needs WIMP info, and info about their car. """
+class Participant(Person):
+    """ Anyone going on a trip needs WIMP info, and info about their car.
+
+    Even leaders will have a Participant record (see docstring of Leader)
+    """
     emergency_info = models.OneToOneField(EmergencyInfo)
     email = models.EmailField(unique=True)
     car = OptionalOneToOneField(Car)
+    affiliation = models.CharField(max_length=1,
+                                   choices=[("S", "Student"),
+                                            ("M", "MIT affiliate"),
+                                            ("N", "Non-affiliate")])
+    attended_lectures = models.BooleanField(default=False)
+    trips_attended = models.ManyToManyField('Trip')
 
-    class Meta(Person.Meta):
-        abstract = True
 
+class Leader(models.Model):
+    """ A Leader is a special participant (just one with extra privileges).
 
-class Leader(TripGoer):
+    This is acheived by pointing to a participant object.
+
+    The same personal + emergency information is required of leaders, but
+    additional fields are present. So, we keep a Participant record for any
+    leader. This makes it easy to check if any participant is a leader (just
+    see `participant.leader`), easy to promote somebody to leader (just make a
+    new leader record, point to their old participant record, etc.)
+
+    It also allows leaders to function as participants (e.g. if a "SC" leader
+    wants to go ice climbing).
+    """
+    participant = models.OneToOneField(Participant)
     rating = models.CharField(max_length=255)  # Leave long for comments about rating
-    car = OptionalOneToOneField(Car)
 
 
 class Trip(models.Model):
@@ -69,15 +88,6 @@ class Trip(models.Model):
     algorithm = models.CharField(max_length='31',
                                  choices=[('lottery', 'lottery'),
                                           ('fcfs', 'first-come, first-serve')])
-
-
-class Participant(TripGoer):
-    affiliation = models.CharField(max_length=1,
-                                   choices=[("S", "Student"),
-                                            ("M", "MIT affiliate"),
-                                            ("N", "Non-affiliate")])
-    attended_lectures = models.BooleanField(default=False)
-    trips_attended = models.ManyToManyField('Trip')
 
 
 class Feedback(models.Model):
