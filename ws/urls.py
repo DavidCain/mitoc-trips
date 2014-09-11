25307e103f22cfd7958a99408d83cefd35e14919
David Cain <davidjosephcain@gmail.com>
2014-09-11 19:24:03 -0400

    Refactor update info to class-based view
    The function was too long and unwieldy. Preserve all of the existing
    logic, but break it into a far more manageable, readable, and modifiable
    class.

--- ws/urls.py
+++ ws/urls.py
@@ -9,8 +9,7 @@ urlpatterns = patterns('',
     url(r'^accounts/$', views.account_home),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^accounts/update_info/', views.update_info),
+    url(r'^accounts/update_info/', views.UpdateParticipantView.as_view()),
     url(r'^accounts/wsc/add_leader/', views.add_leader),
-    url(r'^accounts/wsc/manage_leaders/', views.manage_leaders),
 
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ws/views.py
+++ ws/views.py
@@ -4,30 +4,33 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import modelformset_factory
 from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.utils import timezone
+from django.views.generic import View
 
 from ws import forms
 from ws import models
 
 
-@login_required
-def update_info(request):
-    """ Update stored info for participants or leaders.
-
-    If no such information exists, update it.
-    """
+class UpdateParticipantView(View):
     # The Participant and EmergencyContact are both Person models, have
     # conflicting names. Use prefixes to keep them distinct in POST data
     par_prefix = "participant"
     e_prefix = "emergency_contact"
 
-    # Try to identify if a Participant record already exists
-    try:
-        participant = request.user.participant
-    except ObjectDoesNotExist:
-        participant = None
+    template_name = 'update_info.html'
+    update_msg = 'Personal information updated successfully'
 
-    def _get_forms(request_method):
+    def _has_car(self, request):
+        return 'has_car' in request.POST
+
+    def _participant(self, request):
+        try:
+            return request.user.participant
+        except ObjectDoesNotExist:
+            return None
+
+    def _get_forms(self, request):
         """ Return a dictionary of forms to be passed for template rendering
 
         Outputs three types of forms:
@@ -37,7 +40,8 @@ def update_info(request):
 
         Forms are bound to model instances for UPDATE if such instances exist.
         """
-        post = request.POST if request_method == "POST" else None
+        post = request.POST if request.method == "POST" else None
+        participant = self._participant(request)
 
         # Access other models within participant
         car = participant and participant.car
@@ -45,7 +49,7 @@ def update_info(request):
         e_contact = e_info and e_info.emergency_contact
 
         # If no Participant object, fill at least with User email
-        par_kwargs = {"prefix": par_prefix, "instance": participant}
+        par_kwargs = {"prefix": self.par_prefix, "instance": participant}
         if not participant:
             par_kwargs["initial"] = {'email': request.user.email}
 
@@ -53,52 +57,58 @@ def update_info(request):
             'participant_form':  forms.ParticipantForm(post, **par_kwargs),
             'car_form': forms.CarForm(post, instance=car),
             'emergency_info_form':  forms.EmergencyInfoForm(post, instance=e_info),
-            'emergency_contact_form':  forms.EmergencyContactForm(post, prefix=e_prefix, instance=e_contact),
+            'emergency_contact_form':  forms.EmergencyContactForm(post, prefix=self.e_prefix, instance=e_contact),
         }
         return form_dict
 
-    form_dict = _get_forms(request.method)
+    def get(self, request, *args, **kwargs):
+        form_dict = self._get_forms(request)
+        return render(request, self.template_name, form_dict)
 
-    if request.method == "POST":
+    def post(self, request, *args, **kwargs):
+        form_dict = self._get_forms(request)
         required_dict = form_dict.copy()
 
-        if not 'has_car' in request.POST:
+        if not self._has_car(request):
             required_dict.pop('car_form')
 
         if all(form.is_valid() for form in required_dict.values()):
-            _save_forms(request.user, required_dict)
-            messages.add_message(request, messages.SUCCESS, 'Personal information updated successfully')
+            self._save_forms(request.user, required_dict)
+            messages.add_message(request, messages.SUCCESS, self.update_msg)
             return redirect('/accounts/')
-        # TODO: If POST fails, "this is required" comes up on all car fields
+        else:
+            # TODO: If POST fails, "this is required" displays on all car fields
+            return render(request, self.template_name, form_dict)
 
-    return render(request, 'add_participant.html', form_dict)
+    def _save_forms(self, user, post_forms):
+        """ Given completed, validated forms, handle saving all.
 
+        If no CarForm is supplied, a participant's existing car will be removed.
 
-def _save_forms(user, post_forms):
-    """ Given completed, validated forms, handle saving all.
+        :param post_forms: Dictionary of <template_name>: <form>
+        """
+        e_contact = post_forms['emergency_contact_form'].save()
+        e_info = post_forms['emergency_info_form'].save(commit=False)
+        e_info.emergency_contact = e_contact
+        e_info = post_forms['emergency_info_form'].save()
 
-    If no CarForm is supplied, a participant's existing car will be removed.
+        participant = post_forms['participant_form'].save(commit=False)
+        participant.user = user
+        participant.emergency_info = e_info
+        try:
+            car = post_forms['car_form'].save()
+        except KeyError:  # No CarForm posted
+            # If the Participant already existed and has a stored Car, delete it
+            if participant.car:
+                participant.car.delete()
+                participant.car = None
+        else:
+            participant.car = car
+        participant.save()
 
-    :param post_forms: Dictionary of <template_name>: <form>
-    """
-    e_contact = post_forms['emergency_contact_form'].save()
-    e_info = post_forms['emergency_info_form'].save(commit=False)
-    e_info.emergency_contact = e_contact
-    e_info = post_forms['emergency_info_form'].save()
-
-    participant = post_forms['participant_form'].save(commit=False)
-    participant.user = user
-    participant.emergency_info = e_info
-    try:
-        car = post_forms['car_form'].save()
-    except KeyError:  # No CarForm posted
-        # If the Participant already existed and has a stored Car, delete it
-        if participant.car:
-            participant.car.delete()
-            participant.car = None
-    else:
-        participant.car = car
-    participant.save()
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UpdateParticipantView, self).dispatch(request, *args, **kwargs)
 
 
 @login_required
