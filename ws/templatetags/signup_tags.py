1c7f79d4bfc26f80d24706e3a09395db0e493685
David Cain <davidjosephcain@gmail.com>
2014-09-16 20:06:26 -0400

    Move read-only trip summary to its own template

--- ws/templatetags/signup_tags.py
+++ ws/templatetags/signup_tags.py
@@ -6,4 +6,8 @@ register = template.Library()
 def signup_table(signups, has_notes=False):
     return {'signups': signups, 'has_notes': has_notes}
 
+def trip_summary(trip):
+    return {'trip': trip}
+
 register.inclusion_tag('signup_table.html')(signup_table)
+register.inclusion_tag('trip_summary.html')(trip_summary)
