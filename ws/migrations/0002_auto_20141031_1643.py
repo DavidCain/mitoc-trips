9fce6e9012b3a5135cf70368fe9eb7ba023ba4f6
David Cain <davidjosephcain@gmail.com>
2014-10-31 16:54:42 -0400

    Add migrations for new pairs feature
    Because production-ready version was deployed before the paired lottery
    signups feature, we'll need to perform the first migration. The
    "initial" migration file is the database before this change, and
    migration 0002 is what's needed to accomodate the new feature.
    
    Because we removed `wed_at_noon` from `dateutils`, the initial migration
    file sets the default signup opening time to the new `wed_morning`.
    However, since the default for this field is a Python function (and thus
    will not set a DEFAULT value in the MySQL schema), this is not a problem.

---
+++ ws/migrations/0002_auto_20141031_1643.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import ws.dateutils
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ws', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='lotteryinfo',
+            name='paired_with',
+            field=models.ForeignKey(related_name='paired_by', blank=True, to='ws.Participant', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='trip',
+            name='signups_close_at',
+            field=models.DateTimeField(default=ws.dateutils.wed_morning, null=True, blank=True),
+        ),
+    ]
