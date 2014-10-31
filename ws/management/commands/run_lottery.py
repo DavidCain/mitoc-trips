6685e6d72aa61bc59b730800707427e8ae47d01c
David Cain <davidjosephcain@gmail.com>
2014-10-31 14:08:27 -0400

    Add support for pairs in the lottery algorithm
    Rework participant handling into class-based system (function was
    getting too unwieldy).

--- ws/management/commands/run_lottery.py
+++ ws/management/commands/run_lottery.py
@@ -6,7 +6,6 @@ from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.utils import timezone
-import pytz
 
 from ws import dateutils
 from ws import models
@@ -16,6 +15,9 @@ from datetime import date, timedelta
 today = dateutils.local_now().date()
 jan_1st = date(today.year, 1, 1)
 pytz_timezone = timezone.get_default_timezone()
+
+
+handled = {}  # Key: primary keys, gives boolean if handled
 
 
 class Command(BaseCommand):
@@ -117,54 +119,135 @@ def assign_trips():
         handling_text = "Handling {}".format(participant)
         print handling_text
         print '-' * len(handling_text)
-        handle_participant(participant)
+        par_handler = ParticipantHandler(participant)
+        par_handler.place_participant()
         print
 
 
-def handle_participant(participant):
-    """ Handle assigning a single participant to a trip.
-
-    Each participant is assigned to a maximum of one trip. If no
-    trips are open, they are waitlisted on their highest priority trip.
-    """
+def reciprocally_paired(participant):
     try:
-        driver = participant.lotteryinfo.is_driver
+        paired_par = participant.lotteryinfo.paired_with
+    except ObjectDoesNotExist:  # No lottery info (paired_with default is None)
+        return False
+
+    try:
+        return paired_par and paired_par.lotteryinfo.paired_with == participant
+    except ObjectDoesNotExist:  # No lottery info for paired participant
+        return False
+
+
+def par_is_driver(participant):
+    try:
+        return participant.lotteryinfo.is_driver
     except AttributeError:  # No lottery form submission
-        driver = False
+        return False
 
-    # Only consider lottery signups for future trips
-    signups = participant.signup_set.filter(trip__trip_date__gt=today,
-                                            trip__algorithm='lottery')
-    if not signups:
-        print "{} did not choose any trips this week".format(participant)
-        return
 
-    # Place participant on their first choice available trip
-    for signup in signups.order_by('order', 'time_created'):
-        trip = signup.trip
-        if trip.open_slots:
-            print "{} has {} slot(s), adding {}".format(trip, trip.open_slots, participant)
-            signup.on_trip = True
-            signup.save()
-            break
-        elif driver and not trip.open_slots:
-            # A driver may displace somebody else
-            is_driver = Q(participant__lotteryinfo__car_status__in=['own', 'rent'])
-            participant_drivers = trip.signup_set.filter(is_driver, on_trip=True)
-            lottery_leaders = trip.leaders.filter(participant__lotteryinfo__isnull=False)
-            leader_drivers = sum(leader.participant.lotteryinfo.is_driver
+def place_on_trip(signup):
+    trip = signup.trip
+    print "{} has {} slot(s), adding {}".format(trip, trip.open_slots,
+                                                signup.participant)
+    signup.on_trip = True
+    signup.save()
+
+
+class ParticipantHandler(object):
+    """ Class to handle placement of a single participant or pair. """
+    is_driver_q = Q(participant__lotteryinfo__car_status__in=['own', 'rent'])
+
+    def __init__(self, participant):
+        self.participant = participant
+        self.slots_needed = len(self.to_be_placed)
+
+    @property
+    def is_driver(self):
+        driver = par_is_driver(self.participant)
+        paired_par = self.paired_par
+        return driver or paired_par and par_is_driver(paired_par)
+
+    @property
+    def paired(self):
+        return reciprocally_paired(self.participant)
+
+    @property
+    def paired_par(self):
+        return self.paired and self.participant.lotteryinfo.paired_with
+
+    @property
+    def to_be_placed(self):
+        if self.paired:
+            return (self.participant, self.paired_par)
+        else:
+            return (self.participant,)
+
+    @property
+    def par_text(self):
+        return " + ".join(map(unicode, self.to_be_placed))
+
+    @property
+    def future_signups(self):
+        # Only consider lottery signups for future trips
+        signups = self.participant.signup_set.filter(trip__trip_date__gt=today,
+                                                     trip__algorithm='lottery')
+        if self.paired:  # Restrict signups to those both signed up for
+            signups = signups.filter(trip__in=self.paired_par.trip_set.all())
+        return signups.order_by('order', 'time_created')
+
+    def place_all_on_trip(self, signup):
+        place_on_trip(signup)
+        if self.paired:
+            par_signup = models.SignUp.objects.get(participant=self.paired_par,
+                                                   trip=signup.trip)
+            place_on_trip(par_signup)
+
+    def count_drivers_on_trip(self, trip):
+        participant_drivers = trip.signup_set.filter(self.is_driver_q, on_trip=True)
+        lottery_leaders = trip.leaders.filter(participant__lotteryinfo__isnull=False)
+        num_leader_drivers = sum(leader.participant.lotteryinfo.is_driver
                                  for leader in lottery_leaders)
-            if (participant_drivers.count() + leader_drivers) < 2:
+        return participant_drivers.count() + num_leader_drivers
+
+    def placed_on_trip(self, signup):
+        trip = signup.trip
+        if trip.open_slots >= self.slots_needed:
+            self.place_all_on_trip(signup)
+            return True
+        elif self.is_driver and not trip.open_slots and not self.paired:
+            # A driver may displace somebody else
+            # (but a couple with a driver cannot displace two people)
+            if self.count_drivers_on_trip(trip) < 2:
                 print "{} is full, but doesn't have two drivers".format(trip)
                 print "Adding {} to '{}', as they're a driver".format(signup, trip)
                 add_to_waitlist(lowest_non_driver(trip))
                 signup.on_trip = True
                 signup.save()
-                break
-        else:
-            print "Can't place {} on {}".format(participant, trip)
+                return True
 
-    else:  # No trips are open
-        print "None of {}'s trips are open.".format(participant)
-        favorite_signup = signups.first()
-        add_to_waitlist(favorite_signup)
+    def place_participant(self):
+        if self.paired:
+            print "{} is paired with {}".format(self.participant, self.paired_par)
+            if not handled.get(self.paired_par.id):
+                print "Will handle signups when {} comes".format(self.paired_par)
+                handled[self.participant.id] = True
+                return
+        if not self.future_signups:
+            print "{} did not choose any trips this week".format(self.par_text)
+            handled[self.participant.id] = True
+            return
+
+        # Try to place participants on their first choice available trip
+        for signup in self.future_signups:
+            if self.placed_on_trip(signup):
+                break
+            else:
+                print "Can't place {} on {}".format(self.par_text, signup.trip)
+
+        else:  # No trips are open
+            print "None of {}'s trips are open.".format(self.par_text)
+            favorite_trip = self.future_signups.first().trip
+            for participant in self.to_be_placed:
+                find_signup = Q(participant=participant, trip=favorite_trip)
+                favorite_signup = models.SignUp.objects.get(find_signup)
+                add_to_waitlist(favorite_signup)
+
+        handled[self.participant.id] = True
