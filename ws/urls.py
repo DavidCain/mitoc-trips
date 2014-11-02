a1e5bd80cdcb9eefb08e49bdb074330256e865c2
David Cain <davidjosephcain@gmail.com>
2014-11-02 13:55:36 -0500

    Add a simple "about" page

--- ws/urls.py
+++ ws/urls.py
@@ -51,6 +51,7 @@ urlpatterns = patterns('',
 
     # Help views (most pages available to anyone, some require groups)
     url(r'^help/$', TemplateView.as_view(template_name='help/home.html'), name='help-home'),
+    url(r'^help/about/$', TemplateView.as_view(template_name='help/about.html'), name='help-about'),
     url(r'^help/participants/personal_info/$', TemplateView.as_view(template_name='help/participants/personal_info.html'), name='help-personal_info'),
     url(r'^help/participants/lottery/$', TemplateView.as_view(template_name='help/participants/lottery.html'), name='help-lottery'),
     url(r'^help/participants/signups/$', TemplateView.as_view(template_name='help/participants/signups.html'), name='help-signups'),
