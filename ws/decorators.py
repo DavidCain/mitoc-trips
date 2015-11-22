492cf4ec12b63173f55bd1d7fa8974e200a1e3ad
David Cain <davidjosephcain@gmail.com>
2015-11-22 17:36:34 -0600

    Add utilities module for dealing with permissions

--- ws/decorators.py
+++ ws/decorators.py
@@ -2,25 +2,24 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse_lazy
 
-from ws import models
+from ws import perm_utils
 
 
 #NOTE: Currently excludes the WSC (only the WS chair can assign ratings)
 def chairs_only(*activity_types, **kwargs):
     if not activity_types:
-        activity_types = [val for val, label in models.LeaderRating.ACTIVITIES]
-    chair_groups = {activity + "_chair" for activity in activity_types}
-    return group_required(*chair_groups, **kwargs)
+        activity_types = perm_utils.activity_types
+    groups = {perm_utils.chair_group(activity) for activity in activity_types}
+    return group_required(*groups, **kwargs)
 
 def group_required(*group_names, **kwargs):
     """ Requires user membership in at least one of the groups passed in. """
     # Adapted from Django snippet #1703
     login_url = kwargs.get('login_url', None)
     def in_groups(user):
-        if user.is_authenticated():
-            sudo = kwargs.get('allow_superusers', True) and user.is_superuser
-            if bool(user.groups.filter(name__in=group_names)) or sudo:
-                return True
+        allow_superusers = kwargs.get('allow_superusers', True)
+        if perm_utils.in_any_group(user, group_names, allow_superusers):
+            return True
         if not login_url:
             raise PermissionDenied()
     return user_passes_test(in_groups, login_url=login_url)
---
+++ ws/perm_utils.py
@@ -0,0 +1,20 @@
+from ws import models
+
+
+def chair_group(activity):
+    if activity == 'winter_school':
+        return 'WSC'
+    return activity + '_chair'
+
+def in_any_group(user, group_names, allow_superusers=True):
+    if user.is_authenticated():
+        sudo = allow_superusers and user.is_superuser
+        if bool(user.groups.filter(name__in=group_names)) or sudo:
+            return True
+
+def is_chair(user, activity_type, allow_superusers=True):
+    return in_any_group(user, [chair_group(activity_type)], allow_superusers)
+
+
+activity_types = [val for val, label in models.LeaderRating.ACTIVITIES]
+all_chair_groups = {chair_group(activity) for activity in activity_types}
