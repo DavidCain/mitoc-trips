cd80aa75a808608b19eff707dc948aa63077c035
David Cain <davidjosephcain@gmail.com>
2016-05-17 23:30:10 -0400

    Update the account dropdown
    - Include a Gravatar image
    - Reword some items to be simpler
    - Add the Profile page

--- ws/templatetags/gravatar.py
+++ ws/templatetags/gravatar.py
@@ -14,7 +14,13 @@ def gravatar_url(email, size=40):
 
 
 @register.filter
-def gravatar(email, size=40):
-    url = gravatar_url(email, size * 2)
-    return mark_safe('<img class="img-rounded" src="{}"'
-                     ' height="{size}" width="{size}">'.format(url, size=size))
+def gravatar_square(email, size=30):
+    return gravatar(email, size, False)
+
+@register.filter
+def gravatar(email, size=40, img_rounded=True):
+    kwargs = {'url': gravatar_url(email, size * 2),
+              'size': size,
+              'class': 'class="img-rounded"' if img_rounded else ''}
+    return mark_safe('<img {class} src="{url}"'
+                     ' height="{size}" width="{size}">'.format(**kwargs))
