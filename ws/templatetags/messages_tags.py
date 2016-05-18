ac5928cf0e11955108f4684ac9fda0bef03e5beb
David Cain <davidjosephcain@gmail.com>
2016-05-18 00:09:48 -0400

    Use Bootstrap alert-style messages
    The raw UL doesn't mesh well with the rest of the site's Bootstrap
    stylings.

--- ws/templatetags/messages_tags.py
+++ ws/templatetags/messages_tags.py
@@ -3,11 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('for_templatetags/messages_ul.html', takes_context=True)
-def messages_ul(context):
-    return {'messages': context['messages']}
-
-
 @register.inclusion_tag('for_templatetags/messages_alerts.html', takes_context=True)
 def messages_alerts(context):
     return {'messages': context['messages']}
