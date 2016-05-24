810bff95d6ff42b495c65a25cdda4ec905fe128b
David Cain <davidjosephcain@gmail.com>
2016-05-23 22:54:17 -0400

    Successfully render Bootstrap error alerts
    We weren't mapping the error messages to 'alert-danger' previously. Make
    this more explicit with a template filter.

--- ws/templatetags/messages_tags.py
+++ ws/templatetags/messages_tags.py
@@ -6,3 +6,19 @@ register = template.Library()
 @register.inclusion_tag('for_templatetags/messages_alerts.html', takes_context=True)
 def messages_alerts(context):
     return {'messages': context['messages']}
+
+
+# Map the messages level to the corresponding Bootstrap alert class
+bootstrap_mappings = {
+    'error': 'alert-danger',
+    'success': 'alert-success',
+    'warning': 'alert-warning',
+    'info': 'alert-info',
+}
+
+@register.filter
+def alert_classes(message):
+    bs_tags = ["alert"]
+    if message.tags:
+        bs_tags.append(bootstrap_mappings.get(message.level_tag))
+    return ' '.join(bs_tags)
