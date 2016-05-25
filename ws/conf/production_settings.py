e05966acbfd539ed7ba1cd00f68d1662bce713ea
David Cain <davidjosephcain@gmail.com>
2016-05-24 22:39:40 -0400

    Send email with SES

--- ws/conf/production_settings.py
+++ ws/conf/production_settings.py
@@ -1,5 +1,7 @@
 """ Production settings """
 
+import os
+
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['trips.mitoc.edu', 'mitoc-trips.mit.edu']
@@ -7,10 +9,12 @@ ALLOWED_HOSTS = ['trips.mitoc.edu', 'mitoc-trips.mit.edu']
 ADMINS = (('David Cain', 'davidjosephcain@gmail.com'),)
 
 SERVER_EMAIL = 'no-reply@mitoc.org'
-DEFAULT_EMAIL_FROM = 'trips@mitoc.org'
+DEFAULT_FROM_EMAIL = 'mitoc-trips@mit.edu'
 
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
-# While we configure actual email, this is a useful interim
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = '/webapps/ws/logs/ws_emails.log'
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('SES_USER')
+EMAIL_HOST_PASSWORD = os.getenv('SES_PASSWORD')
