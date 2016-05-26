84281bea10caacf09e74510b3ec762bc5f614aca
David Cain <davidjosephcain@gmail.com>
2016-05-26 01:55:41 -0400

    Correct the participant middleware
    Shouldn't try to query a Participant if anonymous

--- ws/middleware.py
+++ ws/middleware.py
@@ -22,21 +22,13 @@ class PrefetchGroupsMiddleware(object):
 
 
 class ParticipantMiddleware(object):
-    """ On pretty much every view, we will check the user's:
-
-        - Participant object
-        - Leader status
-        - Groups
-
-        To reduce query counts, bake all of this into the core query that
-        generates the user.
-    """
+    """ Include the user's participant (used in most views) """
     def process_request(self, request):
         user = request.user
         if user.is_anonymous():
             request.participant = None
-
-        try:
-            request.participant = models.Participant.objects.get(user_id=user.id)
-        except ObjectDoesNotExist:
-            request.participant = None
+        else:
+            try:
+                request.participant = models.Participant.objects.get(user_id=user.id)
+            except ObjectDoesNotExist:
+                request.participant = None
