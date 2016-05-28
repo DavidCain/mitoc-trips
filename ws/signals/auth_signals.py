e5eeb12252ae41bc8e637b5ec00f45156a07e219
David Cain <davidjosephcain@gmail.com>
2016-05-28 11:02:21 -0400

    Only display the welcome message on login
    There's no reason to take up valuable screen real estate with the same
    static message - only display when a returning user logs in. New users
    aren't familiar with the old interface anyway, so we can omit the
    notification.

--- ws/signals/auth_signals.py
+++ ws/signals/auth_signals.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
+from django.contrib import messages
 from django.contrib.auth.models import Group
+from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
@@ -10,6 +12,15 @@ from ws.models import LeaderRating, Participant
 leaders_group = Group.objects.get(name='leaders')
 users_with_info = Group.objects.get(name='users_with_info')
 
+
+@receiver(user_logged_in)
+def logged_in_message(sender, user, request, **kwargs):
+    if request.participant:
+        messages.info(request, """
+                      Welcome to the new home page! We'll continue to add
+                      features here. Soon, you'll be able to see which items you've
+                      rented from the MITOC office, check your membership status,
+                      and more. """)
 
 @receiver(post_save, sender=LeaderRating)
 def add_leader_perms(sender, instance, created, raw, using, update_fields,
