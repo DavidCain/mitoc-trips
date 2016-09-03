36c6e4409f636edab4c123e121d06c5ae34281df
David Cain <davidjosephcain@gmail.com>
2016-09-03 01:39:22 -0700

    Avoid naming a module 'celery'
    The 'First Steps with Django' guide recommends using a module named
    `celery` and using absolute imports to get around this hack. Remove
    the possibility for confusion altogether and just call the app something
    else.
    
    It was also breaking the import from celery.schedules import crontab in
    the settings file.

--- ws/__init__.py
+++ ws/__init__.py
@@ -2,4 +2,4 @@ default_app_config = 'ws.apps.TripsConfig'
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+from celery_config import app as celery_app
--- ws/celery_config.py
+++ ws/celery_config.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import  # To resolve naming conflict
-
 import os
 
 from celery import Celery
