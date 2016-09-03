d0b5ec524c4b224e675ef1bf7b7792cb5b7a4251
David Cain <davidjosephcain@gmail.com>
2016-09-03 14:13:57 -0700

    Add filter to give instances for choices
    When rendering discounts, we want to display information from
    the models, and the standard CheckboxSelectMultiple widget
    doesn't allow that. Add a filter that allows yielding the
    instance corresponding to a given bound field.
    
    Merge the readonly form tag file into a module of just form tags.

---
+++ ws/templatetags/form_tags.py
@@ -0,0 +1,27 @@
+from django import template
+from copy import copy
+
+register = template.Library()
+
+
+@register.inclusion_tag('for_templatetags/readonly_form.html')
+def readonly_form(form):
+    return {'form': form}
+
+
+@register.filter
+def instances_and_widgets(bound_field):
+    """Returns a list of two-tuples of instances and widgets, designed to
+    be used with ModelMultipleChoiceField and CheckboxSelectMultiple widgets.
+
+    Allows templates to loop over a multiple checkbox field and display the
+    related model instance, such as for a table with checkboxes.
+
+    Usage:
+       {% for instance, widget in form.my_field_name|instances_and_widgets %}
+           <p>{{ instance }}: {{ widget }}</p>
+       {% endfor %}
+    """
+    # Source: http://stackoverflow.com/a/27545910/815632
+    for i, instance in enumerate(bound_field.field.queryset.all()):
+        yield (instance, copy(bound_field[i]))
--- ws/templatetags/readonly_form.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.inclusion_tag('for_templatetags/readonly_form.html')
-def readonly_form(form):
-    return {'form': form}
