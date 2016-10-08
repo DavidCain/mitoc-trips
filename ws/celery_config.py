22795c99b231451b2482f8254ab52a2ed91b2661
David Cain <davidjosephcain@gmail.com>
2016-10-08 16:55:31 -0700

    Fix autodiscovery in Celery config
    Now that we're using app configs as Django best practices dictate (i.e.
    abandoning 'default_app_config' and just marking the AppConfig as an
    INSTALLED_APP), the old way of Celery autodiscovery just doesn't work
    anymore.
    
    For more information, see: http://stackoverflow.com/a/38628748/815632
    
    Also we were using ws/local.py in testing. Looks like this persisted in
    the committed configuration - change it to ws/settings.py

--- ws/celery_config.py
+++ ws/celery_config.py
@@ -2,17 +2,19 @@ import os
 
 from celery import Celery
 
+from django.apps import apps
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ws.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ws.settings')
 
 app = Celery('ws')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
 
 @app.task(bind=True)
