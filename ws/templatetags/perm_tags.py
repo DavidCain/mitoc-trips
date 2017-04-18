2345d16d12b6636f5d90f2784a5490373ac6017b
David Cain <davidjosephcain@gmail.com>
2017-04-18 10:13:59 -0700

    Show admins all the activity chair menus
    As an admin, it's helpful to see outstanding tasks for any given
    activity.

--- ws/templatetags/perm_tags.py
+++ ws/templatetags/perm_tags.py
@@ -10,4 +10,4 @@ register = template.Library()
 @register.filter
 def labeled_chair_activities(user):
     return [choice for choice in models.LeaderRating.CLOSED_ACTIVITY_CHOICES
-            if choice[0] in ws.utils.perms.chair_activities(user)]
+            if choice[0] in ws.utils.perms.chair_activities(user, True)]
