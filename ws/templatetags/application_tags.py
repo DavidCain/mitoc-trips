a1470ca2f75c229e2c61313d3905e7a3aeb106a8
David Cain <davidjosephcain@gmail.com>
2017-05-05 19:20:49 -0400

    Complete display of the climbing application
    I'd left off without finishing the climbing application display for
    activity chairs. This summarizes all the other fields that are currently
    omitted from display.

--- ws/templatetags/application_tags.py
+++ ws/templatetags/application_tags.py
@@ -5,9 +5,20 @@ register = template.Library()
 
 @register.inclusion_tag('for_templatetags/application_details.html')
 def application_details(application):
+    all_fields = application._meta.fields
     text_fields = [
         (field, getattr(application, field.name))
-        for field in application._meta.fields
-        if isinstance(field, TextField)
+        for field in all_fields if isinstance(field, TextField)
     ]
-    return {'text_fields': text_fields}
+
+    familiarities = []
+    lead = 'familiarity_'
+    for field in (f for f in all_fields if f.name.startswith(lead)):
+        if field.name == 'familiarity_sr':
+            short_label = 'self-rescue'
+        else:
+            short_label = field.name[len(lead):].replace('_', ' ')
+        response = getattr(application, 'get_' + field.name + '_display')()
+        familiarities.append((short_label, response))
+
+    return {'familiarities': familiarities, 'text_fields': text_fields}
