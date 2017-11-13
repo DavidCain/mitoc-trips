eaa206b919b19dfbdbed59bb27f10601d2f518f1
David Cain <davidjosephcain@gmail.com>
2017-11-12 16:38:21 -0800

    Query golfing: Shave 70 queries off profile load

--- ws/templatetags/perm_tags.py
+++ ws/templatetags/perm_tags.py
@@ -1,7 +1,7 @@
 from django import template
 
 from ws import models
-import ws.utils.perms
+import ws.utils.perms as perm_utils
 
 
 register = template.Library()
@@ -9,5 +9,6 @@ register = template.Library()
 
 @register.filter
 def labeled_chair_activities(user):
+    chair_activities = set(perm_utils.chair_activities(user, True))
     return [choice for choice in models.LeaderRating.CLOSED_ACTIVITY_CHOICES
-            if choice[0] in ws.utils.perms.chair_activities(user, True)]
+            if choice[0] in chair_activities]
