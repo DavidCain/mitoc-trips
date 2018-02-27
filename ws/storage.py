922e01b220a518ea4648abcb5600096247186e4d
David Cain <davidjosephcain@gmail.com>
2018-02-26 20:15:03 -0800

    Update asset pipeline to fix bug
    The 'chrome://' workaround is no longer needed, but Firebug references a
    file that's not actually included.
    
    This allows us to collect static files once again.

--- ws/storage.py
+++ ws/storage.py
@@ -8,8 +8,8 @@ class CachedStorage(GZIPMixin, PipelineCachedStorage):
                 paths[hashed_name] = (self, hashed_name)
             if isinstance(processed, Exception):
                 message = str(processed)
-                if 'chrome:/' in message:
-                    continue  # Django bug parsing  `url: 'chrome://...'`
                 if 'footable' in message:
                     continue  # V2 required glyphicons with a relative URL...
+                if 'condBorders.png' in message:  # Firebug doesn't actually include
+                    continue
             yield name, hashed_name, processed
