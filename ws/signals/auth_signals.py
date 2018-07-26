47ea07471b066304f93ca94693afa75efab445b4
David Cain <davidjosephcain@gmail.com>
2018-07-25 21:48:42 -0700

    Don't load groups when importing module
    Executing a database query when importing the module is not a good
    design pattern.

--- ws/signals/auth_signals.py
+++ ws/signals/auth_signals.py
@@ -5,14 +5,10 @@ from django.dispatch import receiver
 from ws.models import LeaderRating, Participant
 
 
-leaders_group = Group.objects.get(name='leaders')
-users_with_info = Group.objects.get(name='users_with_info')
-
-
 def update_leader_status(participant):
     leader_ratings = participant.leaderrating_set.filter(active=True)
     if not leader_ratings.exists():
-        leaders_group.user_set.remove(participant.user)
+        Group.objects.get(name='leaders').user_set.remove(participant.user)
 
 
 @receiver(post_save, sender=LeaderRating)
@@ -25,7 +21,8 @@ def modify_leader_perms(sender, instance, created, raw, using, update_fields,
     objects
     """
     if created:
-        leaders_group.user_set.add(instance.participant.user)
+        leaders = Group.objects.get(name='leaders')
+        leaders.user_set.add(instance.participant.user)
     else:  # Updating (maybe to inactive!)
         update_leader_status(instance.participant)
 
@@ -42,9 +39,9 @@ def remove_leader_perms(sender, instance, using, **kwargs):
 
 @receiver(post_save, sender=Participant)
 def has_info(sender, instance, created, raw, using, update_fields, **kwargs):
-    users_with_info.user_set.add(instance.user)
+    Group.objects.get(name='users_with_info').user_set.add(instance.user)
 
 
 @receiver(pre_delete, sender=Participant)
 def no_more_info(sender, instance, using, **kwargs):
-    users_with_info.user_set.remove(instance.user)
+    Group.objects.get(name='users_with_info').user_set.remove(instance.user)
