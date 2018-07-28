DavidCain/mitoc-trips

commit 765f22a9ae01245f4181615a4bc6d604be240c06

For simplicity, only consider WS'ers with signups
We now have over 3,000 participants in the system. Many of them have
long since left MITOC, and showing 2,000 instances of "so and so did not
choose any trips this week" is not useful in the log. Only consider
people who've actually signed up.

Additionally, since the Winter School lottery logic makes a number of
queries (it's not optimized for speed), this should speed things up
notably.

ws/lottery.py
@@ -64,7 +64,12 @@ class WinterSchoolParticipantRanker:
         2. affiliation (MIT affiliated is higher priority)
         3. 'flakiness' (more flakes -> lower priority
         """
-        participants = models.Participant.objects.all()
+        # For simplicity, only look at participants who actually have signups
+        participants = models.Participant.objects.filter(
+            signup__trip__trip_date__gt=self.today,
+            signup__trip__algorithm='lottery',
+            signup__trip__activity='winter_school'
+        )
         return iter(sorted(participants, key=self.priority_key))
 
     def priority_key(self, participant):

commit 5de3bda2d622e69eaad52ef12cf606f8bf3bf368

Fix double-handling of participants in ws lottery
Participants with multiple signups were mistakenly being handled
multiple times.

This was a bug introduced with 765f22a9 (a recent commit, never actually
executed in production).

ws/lottery.py
@@ -69,7 +69,7 @@ class WinterSchoolParticipantRanker:
             signup__trip__trip_date__gt=self.today,
             signup__trip__algorithm='lottery',
             signup__trip__activity='winter_school'
-        )
+        ).distinct()
         return iter(sorted(participants, key=self.priority_key))
 
     def priority_key(self, participant):