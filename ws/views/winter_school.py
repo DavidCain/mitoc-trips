4483dd1a1f9619ef17fa3b3e3d142e7850ba3fea
David Cain <davidjosephcain@gmail.com>
2019-01-10 23:34:47 -0800

    Resolve race condition on lecture sign-in
    It's possible for participants to try to sign in for lectures right at
    the moment the WS chair disables this functionality. Add in a check to
    make sure that, if this does happen, we:
    
    1. Inform the user that something went wrong
    2. Don't return a 500 when FormView.form_invalid can't find a template

--- ws/views/winter_school.py
+++ ws/views/winter_school.py
@@ -3,7 +3,7 @@ Winter School-specific views.
 
 Some views only make sense in the context of Winter School. For example,
 recording lecture attendance or toggling settings that take effect during
-winter school. This module contains those views. The majority of the system
+Winter School. This module contains those views. The majority of the system
 works the same during Winter School as it does during the rest of the year.
 """
 from django.contrib import messages
@@ -19,11 +19,31 @@ from ws.utils.dates import ws_year
 
 class LectureAttendanceView(FormView, LectureAttendanceMixin):
     """ Mark the participant as having attended lectures. """
-
     form_class = forms.AttendedLecturesForm
 
     def get(self, request, *args, **kwargs):
         return redirect(reverse('home'))  # (View lacks its own template)
+
+    def form_invalid(self, form):
+        """ Provide custom behavior on invalidation to compensate for lack of template.
+
+        The default behavior from FormView is to try rendering a template with
+        errors for the user. However, since the attended lectures form is
+        tremendously simple (a hidden input with participant ID, and a submit
+        button), there really isn't any errors we can give to the user.
+
+        We can expect an invalid form submission when:
+        1. A participant loads the attendance form (while self-submission is allowed)
+        2. The Winter School chairs disable lecture sign-in
+        3. The participant then tries to submit the form, but is not allowed
+        """
+        participant = form.cleaned_data['participant']
+
+        # Notifications aren't shown when viewing other participants
+        if participant == self.request.participant:
+            messages.error(self.request, "Unable to record your attendance at this time.")
+
+        return redirect(reverse('view_participant', args=(participant.pk,)))
 
     def form_valid(self, form):
         participant = form.cleaned_data['participant']
@@ -41,7 +61,7 @@ class LectureAttendanceView(FormView, LectureAttendanceMixin):
         if participant == self.request.participant:
             messages.success(self.request, "Marked as having attended lectures!")
 
-        return redirect(reverse('view_participant', args=(participant.id,)))
+        return redirect(reverse('view_participant', args=(participant.pk,)))
 
 
 class WinterSchoolSettingsView(CreateView):
@@ -49,7 +69,7 @@ class WinterSchoolSettingsView(CreateView):
     template_name = 'chair/settings.html'
 
     def get_form_kwargs(self):
-        """ Load existing settingcs. """
+        """ Load existing settings. """
         kwargs = super().get_form_kwargs()
         kwargs['instance'] = models.WinterSchoolSettings.load()
         return kwargs
