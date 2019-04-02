5d95fedc0a771633f5e29cd071d7750e9a2bf062
David Cain <davidjosephcain@gmail.com>
2019-04-01 23:25:06 -0700

    Add a `gapless` template tag
    Often, I write Django logic where rendered newlines present a problem.
    In HTML, newlines don't affect presentation, so extra whitespace is
    perfectly fine. However, when rendering plain txt, newlines affect
    presentation.
    
    Incorporate a Django snippet that allows us to omit gaps in rendered
    nodes.

--- ws/templatetags/general_tags.py
+++ ws/templatetags/general_tags.py
@@ -1,6 +1,36 @@
+import re
+
 from django import template
+from django.template.base import Node
+from django.utils.functional import allow_lazy
 
 register = template.Library()
+
+
+def strip_empty_lines(value):
+    """Return the given HTML with empty and all-whitespace lines removed."""
+    return re.sub(r'\n[ \t]*(?=\n)', '', value)
+
+
+strip_empty_lines = allow_lazy(strip_empty_lines, str)
+
+
+@register.tag
+def gapless(parser, token):
+    """ Remove blank lines between `{% gapless %}` and `{% endgapless %}` """
+    nodelist = parser.parse(('endgapless',))
+    parser.delete_first_token()
+    return GaplessNode(nodelist)
+
+
+class GaplessNode(Node):
+    """ Closely modeled after: https://djangosnippets.org/snippets/569/ """
+
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        return strip_empty_lines(self.nodelist.render(context).strip())
 
 
 @register.filter
--- ws/tests/templatetags/test_general_tags.py
+++ ws/tests/templatetags/test_general_tags.py
@@ -8,3 +8,21 @@ class GeneralTagsTests(SimpleTestCase):
         template_to_render = Template('{% load general_tags %}{{ number|subtract:12 }}')
         rendered_template = template_to_render.render(context)
         self.assertEqual('25', rendered_template)
+
+    def test_strip_empty_lines(self):
+        context = Context({'number': 37})
+        lines = [
+            '{% load general_tags %}',
+            'still newlines here',
+            '{% gapless %}',
+            '{{ number|subtract:12 }}',  # 25, this works with tags!
+            '{% if True %}',
+            '  hello  ',
+            '{% endif %}',
+            '',
+            '',  # Empty lines stripped
+            '{% endgapless %}',
+        ]
+        template_to_render = Template('\n'.join(lines))
+        rendered_template = template_to_render.render(context)
+        self.assertEqual(rendered_template, '\nstill newlines here\n25\n  hello')
