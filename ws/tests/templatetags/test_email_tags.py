6f625b1fed9e13f69def34baacc74001e9c7e277
David Cain <davidjosephcain@gmail.com>
2019-05-18 18:32:39 -0700

    Pipe source HTML through a CSS inliner
    Email clients aren't terribly consistent about applying CSS styles. The
    best way to reliably get the expected formatting is to inline styles on
    each tag. With this commit, I piped the output of our HTML templates
    through a responsive email CSS inliner (https://htmlemail.io/inline/),
    then preserved the Django template tags.

--- ws/tests/templatetags/test_email_tags.py
+++ ws/tests/templatetags/test_email_tags.py
@@ -54,17 +54,29 @@ class EmailTagsTests(TestCase):
         )
         url = f'https://mitoc-trips.mit.edu/trips/{trip.pk}/'
         rendered_html = html_template.render(Context({'trip': trip}))
+
+        inline = {
+            'a': 'color: #3498db; text-decoration: underline;',
+            'p': 'font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;',
+            'h2': 'color: #000000; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0;',
+            'h3': 'color: #000000; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0;',
+            'ul': 'font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;',
+            'li': 'list-style-position: inside; margin-left: 5px;',
+        }
+
         expected_html = '\n'.join(
             [
-                f'<h2><a href="{url}">Some Cool Upcoming Trip</a></h2>',
-                '<h3>Sunday, December 14</h3>',
-                '<p>',
-                '  <ul>',
-                '    <li><strong>Difficulty rating:</strong> Advanced</li>',
-                '      <li><strong>Prerequisites:</strong> Comfort with rough terrain</li>',
-                '      <li><strong>Spaces remaining:</strong> 8</li>',
-                '      <li><strong>Signups close at:</strong> Dec. 13, 2025, 9:30 p.m.</li>',
-                '    <li><strong>Algorithm:</strong> first-come, first-serve</li>',
+                f'<h2 style="{inline["h2"]}">',
+                f'  <a href="{url}" style="{inline["a"]}">Some Cool Upcoming Trip</a>',
+                f'</h2>',
+                f'<h3 style="{inline["h3"]}">Sunday, December 14</h3>',
+                f'<p style="{inline["p"]}">',
+                f'  <ul style="{inline["ul"]}">',
+                f'    <li style="{inline["li"]}"><strong>Difficulty rating:</strong> Advanced</li>',
+                f'      <li style="{inline["li"]}"><strong>Prerequisites:</strong> Comfort with rough terrain</li>',
+                f'      <li style="{inline["li"]}"><strong>Spaces remaining:</strong> 8</li>',
+                f'      <li style="{inline["li"]}"><strong>Signups close at:</strong> Dec. 13, 2025, 9:30 p.m.</li>',
+                f'    <li style="{inline["li"]}"><strong>Algorithm:</strong> first-come, first-serve</li>',
                 '  </ul>',
                 '</p>',
                 '',
