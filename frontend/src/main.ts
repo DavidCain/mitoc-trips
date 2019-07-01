c62f9e523754697b7257d6fd7d48fb55dd570a3f
David Cain <davidjosephcain@gmail.com>
2019-06-30 23:22:13 -0700

    Use Vue with full runtime compiler
    If we wish to inject Vue for use with DOM templates (which is a strict
    requirement for building Vue components in the backend), we'll need the
    full runtime compiler available.
    
    Vue-CLI does not provide this by default.
    
    Also, add a root `div` in our base template, much like Angular listens
    to the `body` tag (because of Vue's virtual DOM, it cannot listen on
    `body`).

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,5 @@
 import Vue from "vue";
-import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+new Vue({}).$mount("#root");
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  // Include the full runtime compiler, which will allow us to parse outerHTML
+  // (Which is necessary on our entrypoint #root, where we read server-generated HTML)
+  // See:
+  // - https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+  // - https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+  runtimeCompiler: true,
   lintOnSave: false
 };
