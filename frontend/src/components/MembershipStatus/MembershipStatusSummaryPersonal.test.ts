0991a7a04b4bb65ee009bf16430c10b411a37bc7
David Cain <davidjosephcain@gmail.com>
2019-07-07 18:31:58 -0700

    Add a component for membership status summary
    When a given user is viewing their membership, they often ask questions
    about the displayed status. Common questions include:
    - Why is no membership found when I just paid/signed a waiver?
    - Why is a waiver expired when I just signed one?
    
    There are common answers to these questions, and some resolutions
    require admin intervention. We provide a simple summary that hopefully
    makes it more clear to people _why_ we display the information that we
    have, and where they can go for help.

---
+++ frontend/src/components/MembershipStatus/MembershipStatusSummaryPersonal.test.ts
@@ -0,0 +1,132 @@
+import Vue from "vue";
+import moment, { Moment } from "moment";
+import { shallowMount, Wrapper } from "@vue/test-utils";
+
+import { setTimeTo } from "@/tests/util";
+
+import { MembershipData } from "@/modules/membership/actions";
+import { MembershipStatus } from "@/modules/membership/status";
+import { localize } from "@/modules/dateutil";
+
+import MembershipStatusSummaryPersonal from "./MembershipStatusSummaryPersonal.vue";
+
+// TODO: Use the TypeScript counterpart for expect.extend to make a custom matcher
+function strippedText(wrapper: Wrapper<Vue>) {
+  return wrapper.text().replace(/\s+/g, " ");
+}
+
+function makeDate(date: string): Moment {
+  return localize(moment(date));
+}
+
+const ACTIVE_MEMBERSHIP = {
+  expires: makeDate("1999-01-23"),
+  active: true,
+  email: "bob@example.com"
+};
+
+const ACTIVE_WAIVER = { expires: makeDate("1999-01-19"), active: true };
+
+describe("current membership - 'Active' and 'Expiring Soon'", () => {
+  let dateNowSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    dateNowSpy = setTimeTo("2025-09-30T12:34:56-05:00");
+  });
+
+  afterAll(() => {
+    dateNowSpy.mockRestore();
+  });
+
+  it("displays membership & waiver expiration when active", () => {
+    const wrapper = shallowMount(MembershipStatusSummaryPersonal, {
+      propsData: {
+        data: {
+          membership: ACTIVE_MEMBERSHIP,
+          waiver: ACTIVE_WAIVER,
+          membershipStatus: "Active"
+        }
+      }
+    });
+    const paragraphs = wrapper.findAll("p");
+    expect(strippedText(paragraphs.at(0))).toEqual(
+      "Your membership is active, and expires on Jan 23, 1999."
+    );
+    expect(paragraphs.at(1).text()).toEqual(
+      "Your waiver will expire on Jan 19, 1999."
+    );
+  });
+
+  it("prompts users to renew soon if their membership is nearing its end", () => {
+    const wrapper = shallowMount(MembershipStatusSummaryPersonal, {
+      propsData: {
+        data: {
+          membership: {
+            expires: makeDate("2025-10-02"),
+            active: true,
+            email: "bob@example.com"
+          },
+          waiver: {
+            expires: makeDate("2025-10-02"),
+            active: true
+          },
+          membershipStatus: "Expiring Soon"
+        }
+      }
+    });
+
+    expect(strippedText(wrapper)).toContain(
+      "Your membership is expiring soon! Renew today to keep your membership valid until Oct 2, 2026."
+    );
+  });
+});
+
+describe("bad membership - 'Expired,' 'Missing,' and 'Missing Membership'", () => {
+  it("tells the user if we've never had a membership before", () => {
+    const wrapper = shallowMount(MembershipStatusSummaryPersonal, {
+      propsData: {
+        data: {
+          membership: { email: null, expires: null, active: false },
+          waiver: { expires: null, active: false },
+          membershipStatus: "Missing"
+        }
+      }
+    });
+    expect(strippedText(wrapper)).toEqual(
+      "We have no membership information on file for any of your verified email addresses. " +
+        "You must become a member and sign a new waiver in order to participate on trips, rent gear, or use cabins."
+    );
+    expect(wrapper.html()).toContain('href="/profile/membership/"');
+    expect(wrapper.html()).toContain('href="/profile/waiver/"');
+  });
+});
+
+describe("bad waiver - 'Missing Waiver' and 'Waiver Expired'", () => {
+  it("prompts active members without a waiver to sign one", () => {
+    const wrapper = shallowMount(MembershipStatusSummaryPersonal, {
+      propsData: {
+        data: {
+          membership: ACTIVE_MEMBERSHIP,
+          waiver: { expires: null, active: false },
+          membershipStatus: "Missing Waiver"
+        }
+      }
+    });
+    expect(wrapper.text()).toEqual("Please sign a waiver.");
+    expect(wrapper.html()).toContain('href="/profile/waiver/"');
+  });
+
+  it("prompts members with an expired waiver to sign one", () => {
+    const wrapper = shallowMount(MembershipStatusSummaryPersonal, {
+      propsData: {
+        data: {
+          membership: ACTIVE_MEMBERSHIP,
+          waiver: { expires: makeDate("1982-11-12"), active: false },
+          membershipStatus: "Waiver Expired"
+        }
+      }
+    });
+    expect(wrapper.text()).toEqual("Please sign a new waiver.");
+    expect(wrapper.html()).toContain('href="/profile/waiver/"');
+  });
+});
---
+++ frontend/src/components/MembershipStatus/MembershipStatusSummaryPersonal.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <p v-if="membershipStatus === 'Missing Waiver'">
+      Please <a href="/profile/waiver/">sign a waiver</a>.
+    </p>
+    <p v-else-if="membershipStatus === 'Waiver Expired'">
+      Please <a href="/profile/waiver/">sign a new waiver</a>.
+    </p>
+    <div v-else-if="membershipStatus === 'Expiring Soon'">
+      <div class="alert alert-warning">
+        <h4>Your membership is expiring soon!</h4>
+        <p>
+          <a href="/profile/membership/">Renew today</a>
+          to keep your membership valid until
+          {{ renewalValidUntil | formatDate }}.
+        </p>
+      </div>
+      <p>
+        Your waiver will expire on {{ data.waiver.expires | formatDate }}.
+        <a href="/profile/waiver/">Sign another?</a>
+      </p>
+    </div>
+    <div v-else-if="membershipStatus === 'Active'">
+      <p>
+        Your membership is active, and expires on
+        {{ data.membership.expires | formatDate }}.
+      </p>
+      <p>Your waiver will expire on {{ data.waiver.expires | formatDate }}.</p>
+    </div>
+    <div v-else-if="membershipStatus === 'Missing Membership'">
+      <p>
+        We have a current waiver on file, but no active membership.
+      </p>
+      <p>
+        You can still participate in mini-trips, but you'll need
+        <a href="/profile/membership/">a full MITOC membership</a>
+        in order to rent gear, use cabins, or join other trips.
+      </p>
+    </div>
+    <div v-else-if="membershipStatus === 'Missing'">
+      <p>
+        We have no membership information on file for any of your
+        <a href="/accounts/email/">verified email addresses.</a>
+      </p>
+
+      <p>
+        You must <a href="/profile/membership/">become a member</a> and
+        <a href="/profile/waiver/">sign a new waiver</a>
+        in order to participate on trips, rent gear, or use cabins.
+      </p>
+    </div>
+    <div v-else-if="membershipStatus === 'Expired'">
+      <p>
+        Your membership expired on {{ data.membership.expires | formatDate }}.
+      </p>
+      <p>
+        Please <a href="/profile/membership/">renew your membership</a> and
+        <a href="/profile/waiver/">sign a new waiver</a>.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import moment, { Moment } from "moment";
+import "@/filters";
+
+import {
+  MembershipStatus,
+  expirationIfRenewingToday
+} from "@/modules/membership/status";
+import { localize } from "@/modules/dateutil";
+import { MembershipData } from "@/modules/membership/actions";
+
+@Component
+export default class MembershipStatusSummaryPersonal extends Vue {
+  @Prop() private data!: MembershipData;
+
+  get membershipStatus(): MembershipStatus {
+    return this.data.membershipStatus;
+  }
+
+  get renewalValidUntil(): Moment {
+    return expirationIfRenewingToday(this.data.membership.expires);
+  }
+}
+</script>
