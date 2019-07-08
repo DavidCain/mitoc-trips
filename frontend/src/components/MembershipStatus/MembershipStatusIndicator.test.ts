f9455fd3b9d9f7fa5f8870255f997f3408f79f60
David Cain <davidjosephcain@gmail.com>
2019-07-07 18:29:30 -0700

    Create a membership status component
    We conditionally display (and stylize) each MITOC member's status.
    Encapsulate this behavior in a single, testable component. It will prove
    useful in a top-level component that obtains and displays information
    about a given user's membership.
    
    It need not be registered globally, since it will only be used by a
    single component.

---
+++ frontend/src/components/MembershipStatus/MembershipStatusIndicator.test.ts
@@ -0,0 +1,45 @@
+import { shallowMount } from "@vue/test-utils";
+import MembershipStatusIndicator from "./MembershipStatusIndicator.vue";
+import { MembershipStatus } from "@/modules/membership/status";
+
+it("renders the active state", () => {
+  const wrapper = shallowMount(MembershipStatusIndicator, {
+    propsData: { membershipStatus: "Active" }
+  });
+  expect(wrapper.text()).toEqual("Active");
+  expect(wrapper.classes()).toEqual(["label", "label-success"]);
+});
+
+it("renders warning statuses", () => {
+  const warningStatuses = [
+    "Waiver Expired",
+    "Missing Waiver",
+    "Missing Membership"
+  ];
+  warningStatuses.forEach(membershipStatus => {
+    const wrapper = shallowMount(MembershipStatusIndicator, {
+      propsData: { membershipStatus }
+    });
+    expect(wrapper.text()).toEqual(membershipStatus);
+    expect(wrapper.classes()).toEqual(["label", "label-warning"]);
+  });
+});
+
+it("renders 'danger' statuses", () => {
+  const warningStatuses = ["Expired", "Missing"];
+  warningStatuses.forEach(membershipStatus => {
+    const wrapper = shallowMount(MembershipStatusIndicator, {
+      propsData: { membershipStatus }
+    });
+    expect(wrapper.text()).toEqual(membershipStatus);
+    expect(wrapper.classes()).toEqual(["label", "label-danger"]);
+  });
+});
+
+it("renders the special 'expiring soon' status", () => {
+  const wrapper = shallowMount(MembershipStatusIndicator, {
+    propsData: { membershipStatus: "Expiring Soon" }
+  });
+  expect(wrapper.text()).toEqual("Expiring Soon");
+  expect(wrapper.classes()).toEqual(["label", "label-info"]);
+});
---
+++ frontend/src/components/MembershipStatus/MembershipStatusIndicator.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="label" v-bind:class="className">
+    {{ membershipStatus }}
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { MembershipStatus } from "@/modules/membership/status";
+
+// Map from each status type to a Bootstrap CSS class
+const statusToBootstrapClass: Record<MembershipStatus, string> = {
+  Active: "label-success",
+  Expired: "label-danger",
+  Missing: "label-danger",
+  "Waiver Expired": "label-warning",
+  "Missing Waiver": "label-warning",
+  "Missing Membership": "label-warning",
+  "Expiring Soon": "label-info" // Special front-end only status
+};
+
+@Component
+export default class MembershipStatusIndicator extends Vue {
+  @Prop() private membershipStatus!: MembershipStatus;
+
+  get className(): string {
+    return statusToBootstrapClass[this.membershipStatus];
+  }
+}
+</script>
