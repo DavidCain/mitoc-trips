d0f2b12fd3bd5ceeb4b8adb6f30b3851807d23ed
David Cain <davidjosephcain@gmail.com>
2019-07-07 18:35:36 -0700

    Replace Angular directive with Vue component
    We've created a Vue component (MembershipStatus) that replaces the
    Angular directive of the same name.
    
    While we're in transition, both Vue and Angular will be watching the DOM
    to see if they should apply changes to a given element. It's important
    to not have any collisions on element names, so we transfer control to
    Vue.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,11 @@
 import Vue from "vue";
 
 import "./filters";
+import MembershipStatus from "./components/MembershipStatus.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({}).$mount("#root");
+new Vue({
+  // TODO: https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
+  components: { MembershipStatus }
+}).$mount("#root");
