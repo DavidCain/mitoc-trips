5d105d10745f644cdfa3d8acf54c6723331b32db
David Cain <davidjosephcain@gmail.com>
2019-08-28 22:24:36 -0700

    Add a very simple test of WSGI
    This script is extremely simple, but it's critical - we should get some
    actual test coverage on it to show that it works.

---
+++ ws/tests/test_wsgi.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+from unittest import mock
+
+from django import core
+
+
+class WsgiTest(unittest.TestCase):
+    def test_exposes_application_in_var(self):
+        """ The WSGI script expects an application available. """
+        with mock.patch.object(
+            core.wsgi, 'get_wsgi_application', wraps=core.wsgi.get_wsgi_application
+        ) as get_app:
+            from ws import wsgi
+
+        get_app.assert_called_once()
+        self.assertTrue(isinstance(wsgi.application, core.handlers.wsgi.WSGIHandler))
+
+        # We also set the Django settings module correctly
+        self.assertEqual(os.environ["DJANGO_SETTINGS_MODULE"], "ws.settings")
