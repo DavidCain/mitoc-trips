41b72b2b6fb9f611db397c02cc8d7516f780e737
David Cain <davidjosephcain@gmail.com>
2019-10-12 22:17:20 -0700

    Add a generic utility to extract form choices
    Django's syntax for grouping choices makes it a little tricky to tell
    when a tuple is a named group or just a label & value pairing. Make a
    generic util to extract just the raw valid choices.

---
+++ ws/tests/utils/test_forms.py
@@ -0,0 +1,72 @@
+import unittest
+
+from ws.utils.forms import all_choices
+
+
+class FormUtilTests(unittest.TestCase):
+    def test_normal_case(self):
+        choices = [
+            ('none', "not at all"),
+            ('some', "some exposure"),
+            ('comfortable', "comfortable"),
+            ('very comfortable', "very comfortable"),
+        ]
+        self.assertEqual(
+            list(all_choices(choices)),
+            ['none', 'some', 'comfortable', 'very comfortable'],
+        )
+
+    def test_mixed_named_groups(self):
+        """ It works mixing named groups & non-groups. """
+
+        choices = [
+            (
+                'Undergraduate student',
+                [('MU', "MIT undergrad"), ('NU', "Non-MIT undergrad")],
+            ),
+            (
+                'Graduate student',
+                [('MG', "MIT grad student"), ('NG', "Non-MIT grad student")],
+            ),
+            (
+                'MIT',
+                [
+                    ('MA', 'MIT affiliate (staff or faculty)'),
+                    ('ML', "MIT alum (former student)"),
+                ],
+            ),
+            ('NA', 'Non-affiliate'),
+        ]
+        self.assertEqual(
+            list(all_choices(choices)), ['MU', 'NU', 'MG', 'NG', 'MA', 'ML', 'NA']
+        )
+
+    def test_nested_groups(self):
+        """ Nested named groups are handled properly. """
+
+        choices = [
+            (
+                'MIT',
+                [
+                    ('Student', [('MU', "MIT undergrad"), ('MG', "MIT grad student")]),
+                    (
+                        'Non-student',
+                        [
+                            ('MA', 'MIT affiliate (staff or faculty)'),
+                            ('ML', "MIT alum (former student)"),
+                        ],
+                    ),
+                ],
+            ),
+            (
+                'Non-MIT',
+                [
+                    ('NA', 'Non-affiliate'),
+                    ('NG', "Non-MIT grad student"),
+                    ('NU', "Non-MIT undergrad"),
+                ],
+            ),
+        ]
+        self.assertEqual(
+            list(all_choices(choices)), ['MU', 'MG', 'MA', 'ML', 'NA', 'NG', 'NU']
+        )
---
+++ ws/utils/forms.py
@@ -0,0 +1,14 @@
+def all_choices(choices):
+    """ Recursively extract the choices from a potentially nested set of choices. """
+    for left, right in choices:
+        if isinstance(right, str):
+            # Right side is the label, left side is the value
+            yield left
+            continue
+
+        # Left side is a name for the group.
+        # Right side is either:
+        # - another named group (nesting up to twice)
+        # - an iterable of (value, label) tuples
+        for choice in all_choices(right):
+            yield choice
