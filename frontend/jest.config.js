8bc469d93d8904fc4182876790bb1abd596c0055
David Cain <davidjosephcain@gmail.com>
2019-12-22 15:33:32 -0800

    Update to Vue CLI 4
    This major upgrade gets us onto Jest 24 and includes a lot of great
    fixes for Vue CLI 3.
    
    https://cli.vuejs.org/migrating-from-v3/#vue-cli-plugin-unit-jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,25 +1,4 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "vue", "ts", "tsx"],
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  transformIgnorePatterns: ["/node_modules/"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
-  },
-  snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: ["**/**.test.ts|**/__tests__/*.(ts|tsx)"],
-  testURL: "http://localhost/",
-  watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
-  ],
-  globals: {
-    "ts-jest": {
-      babelConfig: true
-    }
-  }
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["**/**.test.ts|**/__tests__/*.(ts|tsx)"]
 };
