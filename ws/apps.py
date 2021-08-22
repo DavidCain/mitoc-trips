7dc244ca4c2c71ec06798643490bc18d135b0d72
David Cain <davidjosephcain@gmail.com>
2021-08-22 10:28:06 -0700

    Remove the obsolete test config
    This configuration was used for a little while to disable signals in
    unit tests. In hindsight, using signals at all was probably a mistake
    (they're a fraught pattern). In any case, we haven't used the TestConfig
    since 2017, when it was removed with 0e97aa09b95ced503b05f994f2bc675adb1a33f8

--- ws/apps.py
+++ ws/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestConfig(AppConfig):
-    """A simple configuration that doesn't load signals."""
-
-    name = 'ws'
-    verbose_name = "MITOC Trips"
-
-
 class TripsConfig(AppConfig):
     name = 'ws'
     verbose_name = "MITOC Trips"
