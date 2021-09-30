14da0a172b8f89463026a5fe38f0a227cc58b70a
David Cain <davidjosephcain@gmail.com>
2021-09-29 18:36:07 -0700

    Simplify the "active WIMPs" query
    This method was formerly much more complicated because it had to span
    two different databases (`auth_db` and `ws`). We can simplify it by
    changing to a single SQL query.
    
    Additionally, this solves a deprecation warning that will be raised once
    we switch to Django 3.1. The ability to pass a raw SQL expression to an
    `order_by` clause is going away in Django 4.0.

--- ws/wimp.py
+++ ws/wimp.py
@@ -1,29 +1,20 @@
-from django.contrib.auth.models import Group
+from typing import Optional
+
+from django.db.models.expressions import RawSQL
 
 from ws import models
 
 
 def active_wimps():
-    """Yield all Participants that are currently WIMPs.
+    """Return all Participants that are currently WIMPs.
 
     Generally speaking, we should have only one WIMP, but we should handle the
     case of there being more than one, since the data model supports that.
     """
-    users = Group.objects.get(name='WIMP').user_set
-    recent_wimps = users.order_by('-auth_user_groups.id')  # Most recent first
-    # NOTE: Coerced to a list so these queries can be executed across dbs
-    wimp_users = list(recent_wimps.values_list('id', flat=True))
-    par_by_user_id = {
-        par.user_id: par
-        for par in models.Participant.objects.filter(user_id__in=wimp_users)
-    }
-    for user_id in wimp_users:
-        if user_id in par_by_user_id:
-            yield par_by_user_id[user_id]
+    return models.Participant.objects.filter(user__groups__name='WIMP').order_by(
+        RawSQL('auth_user_groups.id', ()).desc()
+    )
 
 
-def current_wimp():
-    try:
-        return next(active_wimps())
-    except StopIteration:
-        return None
+def current_wimp() -> Optional[models.Participant]:
+    return active_wimps().first()
