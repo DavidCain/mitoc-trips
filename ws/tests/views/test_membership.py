DavidCain/mitoc-trips

commit b687472b63f202546c269814b1b5f8a3dbb8b135

Correctly communicate renewal terms
With the new membership reminder emails, we're telling users that they
can pay dues early and have the remaining time added on to their next
membership. However, we were stating that "membership lasts for 365 days
after dues are paid," which is very misleading.

Avoid contradicting the messaging.

ws/models.py
@@ -194,9 +194,9 @@ class Membership(models.Model):
     last_cached = models.DateTimeField(auto_now=True)
 
     @property
-    def membership_active(self):
+    def membership_active(self) -> bool:
         expires = self.membership_expires
-        return expires and expires >= date_utils.local_date()
+        return bool(expires) and expires >= date_utils.local_date()
 
     def should_sign_waiver_for(self, trip):
         """Return if the waiver will be valid for the day of the trip.
@@ -242,7 +242,22 @@ class Membership(models.Model):
 
         return earliest_renewal_date
 
-    def should_renew_for(self, trip):
+    @property
+    def in_early_renewal_period(self) -> bool:
+        """Return if the member is in their last ~40 days of membership, can renew."""
+        renewal_date = self.date_when_renewal_is_recommended(report_past_dates=True)
+        return bool(renewal_date) and renewal_date <= date_utils.local_date()
+
+    @property
+    def expiry_if_paid_today(self) -> date:
+        """Return the date which membership would expire, if paid today."""
+        if not self.in_early_renewal_period:
+            return date_utils.local_date() + timedelta(days=365)
+
+        assert self.membership_expires
+        return self.membership_expires + timedelta(days=365)
+
+    def should_renew_for(self, trip) -> bool:
         """Return if membership renewal is required to attend a future trip.
 
         If a participant's membership will expire on the given date, and it's

ws/tests/views/test_membership.py
@@ -1,7 +1,11 @@
+from datetime import date
 from unittest import mock
 
+from bs4 import BeautifulSoup
+from freezegun import freeze_time
+
 from ws import forms, waivers
-from ws.tests import TestCase, factories
+from ws.tests import TestCase, factories, strip_whitespace
 
 
 class PayDuesTests(TestCase):
@@ -62,6 +66,28 @@ class PayDuesTests(TestCase):
         self.assertEqual(form.fields['merchantDefinedData1'].initial, 'membership')
         self.assertEqual(form.fields['merchantDefinedData2'].initial, 'MA')
         self.assertEqual(form.fields['merchantDefinedData3'].initial, 'tim@mit.edu')
+
+    @freeze_time("2021-12-10 12:00:00 EST")
+    def test_load_form_as_member_able_to_renew(self):
+        """We clearly communicate when membership ends if you renew."""
+        par = factories.ParticipantFactory.create(
+            membership__membership_expires=date(2021, 12, 25)
+        )
+        self.assertTrue(par.membership.in_early_renewal_period)
+        self.client.force_login(par.user)
+
+        response = self.client.get('/profile/membership/')
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        lead_par = soup.find('p', class_='lead')
+        self.assertEqual(
+            lead_par.text, 'To make the most of MITOC, you must be an active member.'
+        )
+        self.assertEqual(
+            strip_whitespace(lead_par.find_next('p').text),
+            'Renewing today keeps your membership active until Dec 25, 2022. '
+            "Membership enables you to rent gear from the office, participate in upcoming trips, and stay at MITOC's cabins.",
+        )
 
     def test_pay_anonymously(self):
         """Users need not log in to pay dues."""

ws/tests/models/test_membership.py
@@ -30,10 +30,20 @@ class MembershipActiveTests(SimpleTestCase):
 
 @freeze_time("2019-10-19 12:00:00 EST")
 class ShouldRenewForTripTests(SimpleTestCase):
-    @staticmethod
-    def _should_renew_for(trip, membership_expires):
+    def _should_renew_for(self, trip, membership_expires) -> bool:
         membership = models.Membership(membership_expires=membership_expires)
-        return membership.should_renew_for(trip)
+        should_renew = membership.should_renew_for(trip)
+
+        # Test some nice helper methods while we're at it
+        if should_renew:
+            # We should only recommend renewal if it's possible
+            if membership_expires:
+                self.assertTrue(membership.in_early_renewal_period)
+
+            # First-time members *or* renewals should *always* be later than the trip
+            self.assertGreater(membership.expiry_if_paid_today, trip.trip_date)
+
+        return should_renew
 
     def test_mini_trips_not_covered(self):
         """Membership never needs to be renewed for 'mini trips'"""

ws/templates/profile/membership.html
@@ -6,11 +6,15 @@
 {% block content %}
   <h1>Pay MITOC membership dues</h1>
 
-  <p class="lead">
-    To make the most of MITOC, you must be an active member.
-  </p>
+  <p class="lead">To make the most of MITOC, you must be an active member.</p>
   <p>
-    Membership lasts for 365 days after dues are paid, and enables you to
+    {% if viewing_participant.membership.in_early_renewal_period %}
+      Renewing today keeps your membership active until {{ viewing_participant.membership.expiry_if_paid_today|date:"M j, Y" }}.
+      Membership
+    {% else %}
+      Membership lasts for 365 days after dues are paid, and
+    {% endif %}
+    enables you to
     <a href="https://mitoc.mit.edu/rentals">rent gear</a> from the office,
     participate in <a href="{% url 'upcoming_trips' %}">upcoming trips</a>,
     and stay at <a href="https://mitoc.mit.edu/rentals/cabins">MITOC's cabins</a>.

commit be2c21f8ad06785d9e877b996715712a94fc8b32

Fix a bug in communicating renewal dates
A recent commit attempted to better communicate the terms of membership
renewal to those who are presently in the early renewal period, but an
incorrect boolean argument botched the logic.

This was noticed by a MITOC member whose membership had lapsed, and we
were incorrectly telling them that renewing today would give them 1
*month* of membership. Oops.

This fixes a bug introduced in b687472b63f202546c269814b1b5f8a3dbb8b135

ws/models.py
@@ -245,6 +245,8 @@ class Membership(models.Model):
     @property
     def in_early_renewal_period(self) -> bool:
         """Return if the member is in their last ~40 days of membership, can renew."""
+        if not self.membership_active:
+            return False
         renewal_date = self.date_when_renewal_is_recommended(report_past_dates=True)
         return bool(renewal_date) and renewal_date <= date_utils.local_date()
 

ws/tests/views/test_membership.py
@@ -89,6 +89,29 @@ class PayDuesTests(TestCase):
             "Membership enables you to rent gear from the office, participate in upcoming trips, and stay at MITOC's cabins.",
         )
 
+    @freeze_time("2021-12-10 12:00:00 EST")
+    def test_load_form_as_lapsed_member(self):
+        par = factories.ParticipantFactory.create(
+            membership__membership_expires=date(2021, 1, 2)
+        )
+        self.assertFalse(par.membership.in_early_renewal_period)
+        self.assertEqual(par.membership.expiry_if_paid_today, date(2022, 12, 10))
+
+        self.client.force_login(par.user)
+
+        response = self.client.get('/profile/membership/')
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        lead_par = soup.find('p', class_='lead')
+        self.assertEqual(
+            lead_par.text, 'To make the most of MITOC, you must be an active member.'
+        )
+        self.assertEqual(
+            strip_whitespace(lead_par.find_next('p').text),
+            "Membership lasts for 365 days after dues are paid, and enables you to "
+            "rent gear from the office, participate in upcoming trips, and stay at MITOC's cabins.",
+        )
+
     def test_pay_anonymously(self):
         """Users need not log in to pay dues."""
         valid_form_data = {